d6e5a8e177d53a8a61435261c0e9531041c1de7c
a.memon <ahmed.shiraz.memon@gmail.com>
2013-07-02 18:07:59 +0200

    comitting missing status class

---
+++ src/main/java/eu/emi/emir/ui/data/Status.java
@@ -0,0 +1,6 @@
+package eu.emi.emir.ui.data;
+
+public class Status {
+	public static final String numberOfEntries = "NumberofEntries";
+	public static final String serverVersion = "EMIRServerVersion";
+}
